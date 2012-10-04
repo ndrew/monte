a067a5cb4e6455f5159297e4ac007c365c71454d
Andrew Sernyak <ndrew.sernyak@gmail.com>
2012-10-04 18:49:30 +0300

    hello world!

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject monte "0.0.0-SNAPSHOT"
+  :description "Monte is a software visualisation tool created as 
+            	a part of Andrew Sernyak master degree' thesis"
+  :url "https://github.com/ndrew/monte"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.4.0"]])
