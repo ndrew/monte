c4f169e5b8ce422bffa2c3cee0b611a7e1127b2f
Andrew Sernyak <ndrew.sernyak@gmail.com>
2012-10-23 09:06:32 +0300

    added dependencies and set monte.launcher as project entry point. ClojureScript is next to be added

--- project.clj
+++ project.clj
@@ -5,5 +5,31 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [fs "1.2.0"]]
-  :main monte.core)
+                 [fs "1.2.0"]
+                 [noir "1.3.0-beta1"]
+                 [fetch "0.1.0-alpha2"]
+                 [crate "0.2.1"]
+                 [jayq "0.1.0-alpha4"]
+                 ]
+;  :plugins [[lein-cljsbuild "0.2.9"]]
+;  :hooks [leiningen.cljsbuild]
+;  :cljsbuild {
+;    :builds [{
+;        :jar true
+;		:source-path "src-cljs"
+;        :compiler {
+;            :output-to "resources/public/js/cljs.js"
+;            :optimizations :whitespace
+;            :pretty-print true}
+;    }]}
+
+
+
+:repl-options { 
+    :init-ns monte.launcher 
+    :init (do
+      ;; todo: add some hacks later
+    )}
+
+
+  :main monte.launcher)
