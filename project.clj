8ab4380ebd8b89cad94136a8a0e7e602692a4722
Andrew Sernyak <ndrew.sernyak@gmail.com>
2012-11-06 23:59:14 +0200

    struggling with dependencies

--- project.clj
+++ project.clj
@@ -7,8 +7,9 @@
   
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [fs                  "1.2.0"]
-                 [noir                "1.3.0-beta3" 
+                 [noir                "1.3.0-beta1" 
                     :exclusions [org.clojure/clojure]]
+                 [hiccup              "1.0.1"]
                  ; <?> why we need this?
                  [crate               "0.2.1"]
                  [jayq                "0.1.0-alpha4"]
