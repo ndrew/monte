8743548b9b2f7f3692568235f8d0acb3ed0b591a
Andrew Sernyak <ndrew.sernyak@gmail.com>
2013-01-07 16:28:25 +0200

    progress with project view — started with miner and variable lists

--- src/monte/views/index.clj
+++ src/monte/views/index.clj
@@ -30,16 +30,42 @@
     [:article {:id "project"
                :style "background-color: rgba(255,255,128,0.5);"}
     
-      [:h1 {:style "display: inline;"} project] 
-      [:h2 {:style "display: inline; padding-left: 1.5em;"}
+      [:h1 {:style "display: block;"} project] 
+      [:h2 {:style "display: block; padding-left: 1.5em;"}
          " 1. " [:a {:href "#" :id "miner"} "Miners"] 
          " 2. " [:a {:href "#" :id "refine"} "Refine data"] ; todo: better naming
-         " 3. " [:a {:href "#" :id "visualization"} "Visualization"]]]
-      [:div {:id "miner_view" :style "display: none;"} "miners"]
-      [:div {:id "refine_view" :style "display: none;"} "refinements"]
-      [:div {:id "visualization_view" :style "display: none;"} "visualization"]
+         " 3. " [:a {:href "#" :id "visualization"} "Visualization"]]
       
+      [:div {:id "miner_view" :class "view"}
+            [:p "Please select and configure miners. " 
+            [:span {:class "hint"} "(?)"]]
+            
+            [:div {:class "list"}
+                  [:h4 "Miners"]
+                  [:table
+                         [:tr [:td {:class="new" :colspan="3"}
+                                   [:a {:href "#"} "add new"]]]]]
+
+            [:div {:class "list"}
+                  [:h4 "Variables"]
+                  [:table
+                         [:tr [:td {:class="new" :colspan="3"}
+                                   [:a {:href "#"} "add new"]]]]]
+
+                        
+                  
+      ]
       
+    
+    
+      [:div {:id "refine_view" :class "view"} 
+            [:p "Connect the data sets from miners. " 
+            [:span {:class "hint"} "(?)"]]]
+      
+      [:div {:id "visualization_view" :class "view"}
+            [:p "Define the views and configure data presentation. " 
+            [:span {:class "hint"} "(?)"]]]]
+
       
       
     ; just some info for debugging
