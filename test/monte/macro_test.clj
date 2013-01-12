8a839402ac39dbf1d4e3a25b5e6cbfaf3fea44da
Andrew Sernyak <ndrew.sernyak@gmail.com>
2013-01-12 18:05:29 +0200

    tidying up

--- test/monte/macro_test.clj
+++ test/monte/macro_test.clj
@@ -1,50 +1,6 @@
 (ns monte.macro-test
-  (:require [monte.miners.dummy :as test-ns
-             monte.miners.core :miners])
-  (:use clojure.test
-        ))
-;
+  (:use clojure.test))
  
- 
-(comment         
-(defprotocol Foo (f[x] "")(g[x] ""))
-
-(defmacro from-ns[nmsps & body] 
-  `(binding 
-     [*ns* (find-ns ~nmsps)] 
-       (eval
-          (quote (do ~@body))
-            )))
-
-(deftest from-ns-test
-    
-    (from-ns 'monte.miners.dummy (def test-from-ns :test))  
-      ;(println (ns-publics 'monte.miners.dummy))
-    
-    (is (= :test @(ns-resolve (find-ns 'monte.miners.dummy) 'test-from-ns)))
-)
-
-
-(defmacro defminer1[a & body] 
-  `(from-ns 'monte.miners.dummy
-    	(deftype ~a [])   
-     	(extend-type ~a Foo ~@body)   
-))
- 
-
-(deftest ttt
-  (defminer1 mnr (f[x] :hello)(g[x] :world))
-      ;(println (ns-publics 'monte.miners.dummy))
-    
-  (let [miner ((ns-resolve (find-ns 'monte.miners.dummy) '->mnr))]
-    (is (= :hello (f miner)))
-    (is (= :world (g miner)))
-     
-  )
-
-  ;(defminer1 mnr1 (f[x] :hello)(g[x] :world))
-
-      ; todo: 
-)
-
+(deftest macro-test
+  ; macro tests go here
 )
