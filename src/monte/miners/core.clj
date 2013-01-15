568b13e7d242e2ccac5e9c99e7f8afa0089e12ee
Andrew Sernyak <ndrew.sernyak@gmail.com>
2013-01-15 22:31:09 +0200

    added docs for monte.miners.core

--- src/monte/miners/core.clj
+++ src/monte/miners/core.clj
@@ -1,25 +1,50 @@
 (ns monte.miners.core
   (:use monte.miners.impl)
   (:gen-class))
+
+
+(defmacro from-ns[nmsps & body] 
+  "launches body from namespace"
+  `(binding 
+     [*ns* (find-ns ~nmsps)] 
+       (eval
+          (quote (do ~@body)))))
+
  
 (defprotocol Miner
   "Miner abstraction"
   (f [this] "run miner")
   (get-schema[this] "return a scheme(empty configuration) for miner"))
 
-(defmacro from-ns[nmsps & body] 
-  `(binding 
-     [*ns* (find-ns ~nmsps)] 
-       (eval
-          (quote (do ~@body))
-            )))
 
-(defmacro defminer[a & body] 
+(defmacro defminer[miner-name & body]
+  "defines a miner"
   `(from-ns 'monte.miners.impl
-      (deftype ~a [~'config])   
-      (extend-type ~a Miner ~@body)   
-))
+      (deftype ~miner-name [~'config])   
+      (extend-type ~miner-name Miner ~@body)))
 
+
+(defmacro list-types-implementing[protocol] ; macro as I thought it would find test namespace
+  "list all types that implement specified protocol in miner-ns" ; todo: refactor
+  `(remove nil? 
+     (map #(let [[k# v#] %
+         [_# miner-ns# miner-fn#] (re-find #"(.*)\.(.*)$" (.getName k#))]
+      (cond (not (nil? (find-ns (symbol miner-ns#))))
+        [k# (ns-resolve (find-ns (symbol miner-ns#)) (symbol miner-fn#))]
+        :else 
+          (do 
+            (println (str "Can't load " miner-ns# "/->" miner-fn#))
+            nil)
+      )
+     ) (:impls ~protocol))))
+
+
+(defn load-extern-miners[path]
+  "loads miners from file path"
+  (binding [*ns* (find-ns 'monte.miners.core)] (load-file path))) 
+
+;;;;;;;;;;;;
+; miner impls
 
 (defminer DummyMiner      
   (f [this]     
@@ -29,23 +54,5 @@
   (get-schema[this] 
     {:data :everything}))
 
-; macro as I thought it would find test namespace
-
-(defmacro list-types-implementing[protocol]
-  `(remove nil? (map #(let [[k# v#] %
-               [_# miner-ns# miner-fn#] (re-find #"(.*)\.(.*)$" (.getName k#))]
-            (cond (not (nil? (find-ns (symbol miner-ns#))))
-              [k# (ns-resolve (find-ns (symbol miner-ns#)) (symbol miner-fn#))]
-              :else 
-                (do 
-                  (println (str "Can't load " miner-ns# "/->" miner-fn#))
-                  nil)
-            )
-           ) (:impls ~protocol)))
-  )
-
-
-(defn load-extern-miners[path]
-  (binding [*ns* (find-ns 'monte.miners.core)] (load-file path))
-) 
+;(def)
 
