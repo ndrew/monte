673f52e2b369533fa576b0b834881b3fc8568ef8
Andrew Sernyak <ndrew.sernyak@gmail.com>
2013-01-16 03:43:50 +0200

    honey! I've broken the build

---
+++ src/monte/dummies.clj
@@ -0,0 +1,14 @@
+(ns monte.dummies
+  "Holds canned data for diploma presentation")
+
+
+;;;;;;;;;
+; MINERS
+
+; dummy data for JIRA miner
+(def tasks [{:id "TASK-0: initial task" 
+             :assigne "Someone Else <some@one.else>"}
+            {:id "TASK-1: do something"
+             :assigne "Andrew Sernyak <ndrew.sernyak@gmail.com>"}
+            {:id "TASK-2: do something else"
+             :assigne "Ihor Lozynsky <aigooor@gmailc.com>"}])
