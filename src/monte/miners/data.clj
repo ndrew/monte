2ad9361d1601ef3cb6507293abc9e2ef129fdb84
Andrew Sernyak <ndrew.sernyak@gmail.com>
2013-04-25 01:01:50 +0300

    another version of smart-merge

--- src/monte/miners/data.clj
+++ src/monte/miners/data.clj
@@ -4,12 +4,18 @@
             [clojure.core.reducers :as r]
             [clojure.set :as s]))
 
+; thanx to http://www.thebusby.com/2012/07/tips-tricks-with-clojure-reducers.html
+
+(defn fold-into-vec [coll]
+  "Provided a reducer, concatenate into a vector.                                                                                                                                                                                                                                         
+   Note: same as (into [] coll), but parallel."
+  (r/fold (r/monoid into vector) conj coll))
+
 
 (defn p-smart-merge[data config]
   "Uses core.reducers/fold to return a hash-map produced from data(list of hash-maps) that were merged 
   with function(s) specified in config as {:key-fn (fn[resulting-map current-item])}"
   (let [f (fn [memo item] ; merge function for reducers/fold
-              ;(println "fn " memo item)
               (reduce #(let [ [k v] %2
                               merge-f-id (keyword (str (name k) "-fn"))]
                           (if-let [merge-f (get %1 merge-f-id)]
@@ -19,6 +25,15 @@
                   (r/monoid f (constantly config)) f data)]
     
     (apply dissoc result (filter #(.endsWith (name %) "-fn") (keys config)))))
+    
+    
+(defn p-smart-merge-v [data config]
+  "smart merge via vector config"
+  (let [f (fn [memo item] ; merge function for reducers/fold
+              (merge memo (reduce (fn [i f1] (f1 memo i)) 
+                                  item config)))]
+      (r/fold 
+            (r/monoid f hash-map) f data)))
     
   
 ; awaiting better times
@@ -52,3 +67,50 @@
        (if-let [memo-v (get memo k)]
           (assoc memo k (monoid memo-v item-v))
           (assoc memo k (monoid (monoid) item-v))))))
+
+
+(defn filter-by-f[filter-f] 
+  "parallel filter func"
+    (r/filter filter-f))
+
+
+; other util stuff
+
+
+
+(defn fold-into-sorted-map-desc [coll]
+  "Provided a reducer, concatenate into a vector.                              
+   Note: same as (into [] coll), but parallel."
+   
+   ; todo: for map — (vec coll)
+   ; for other colls?
+  (r/fold (r/monoid conj #(sorted-map-by 
+                            (fn [key1 key2] (compare key2 key1)))) (vec coll)))
+        
+        
+; todo!
+;(defn fold-into-vec-format [coll f]
+;  "Provided a reducer, concatenate into a vector.                                                                                                                                                                                                                                         
+;   Note: same as (into [] coll), but parallel."
+;  (r/fold (r/monoid #(into (f %1) %2) vector) conj coll))
+
+
+
+(defn rindex-by-fn
+  "Uses core.reducers/fold to return a map indexed by the value of key-fn                                                                                                                                                                                                                 
+   applied to each element in coll.                                                                                                                                                                                                                                                       
+   Note: key-fn can return a collection of multiple keys."
+  ([key-fn coll ] (rindex-by-fn key-fn identity coll))
+  ([key-fn value-fn coll] (doall (clojure.core.reducers/fold
+                                  ;; combinef                                                                                                                                                                                                                                             
+                            (clojure.core.reducers/monoid (partial merge-with into)
+                                          hash-map)
+                                  ;; reducef                                                                                                                                                                                                                                              
+                                  (fn [ndx elem] (let [key (key-fn elem)]
+                                                   (if (coll? key)
+                                                     (reduce (fn [sndx selem] (assoc sndx selem (conj (get sndx selem [])
+                                                                                                      (value-fn elem))))
+                                                             ndx key)
+                                                     (assoc ndx key (conj (get ndx key [])
+                                                                    (value-fn elem))))))
+                            coll))))
