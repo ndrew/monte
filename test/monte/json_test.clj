9542010ba85926405889444ed2c1ce8aed1073cc
Andrew Sernyak <ndrew.sernyak@gmail.com>
2013-04-30 00:38:06 +0300

    commented json test

--- test/monte/json_test.clj
+++ test/monte/json_test.clj
@@ -2,7 +2,9 @@
   (:use clojure.test)
   (:require [clojure.data.json :as json]
             [clojure.core.reducers :as r]
-            [clojure.set :as s]))
+            [clojure.set :as s]
+            [cheshire.core :refer :all]
+            [monte.miners.data :refer :all]))
 
 
 (defn count-data[data]
@@ -35,55 +37,6 @@
 
 
 
-(defn p-smart-merge[data config]
-  "Uses core.reducers/fold to return a hash-map produced from data(list of hash-maps) that were merged 
-  with function(s) specified in config as {:key-fn (fn[resulting-map current-item])}"
-  (let [f (fn [memo item] ; merge function for reducers/fold
-              ;(println "fn " memo item)
-              (reduce #(let [ [k v] %2
-                              merge-f-id (keyword (str (name k) "-fn"))]
-                          (if-let [merge-f (get %1 merge-f-id)]
-                            (merge-f %1 item) %1)) 
-              memo item))
-        result (r/fold 
-                  (r/monoid f (constantly config)) f data)]
-    
-    (apply dissoc result (filter #(.endsWith (name %) "-fn") (keys config)))))
-    
-  
-; awaiting better times
-; (defn p-sequential-smart-merge [data config] ; as vector )
-    
-
-(defn merger-by-key
-  "[{key-id value, ...}, {key-id value1, ...}] => {key-id [{...}, {...}]}"
-  
-([key-id]
-  (fn [memo item] ; find a way to autogenerate it 
-     (let [k (get item key-id)
-           filtered-item (dissoc item key-id)]
-      (if-let [v (get memo k)]
-          (assoc memo k (conj v filtered-item))
-          (assoc memo k [filtered-item])))))
-([key-id f];ilter
- (fn [memo item] ; find a way to autogenerate it 
-     (let [k (get item key-id)
-           filtered-item (f item)]
-      (if-let [v (get memo k)]
-          (assoc memo k (conj v filtered-item))
-          (assoc memo k [filtered-item])))))
-)
-
-
-
-
-(defn aggregator-by-key[k monoid]
-  "[{key-id value, ...}, {key-id value1, ...}] => {key-id (f value, value1 ...)}"
-  (fn [memo item] ; find a way to autogenerate it
-     (let [item-v (get item k)]
-       (if-let [memo-v (get memo k)]
-          (assoc memo k (monoid memo-v item-v))
-          (assoc memo k (monoid (monoid) item-v))))))
 
 
 #_(def d (time (p-smart-merge [{:time 100 :sql "sql1"}
@@ -118,116 +71,96 @@
 ;(dorun d1);(println d1)
 
 
-; thanx to http://www.thebusby.com/2012/07/tips-tricks-with-clojure-reducers.html
-
-(defn fold-into-vec [coll]
-  "Provided a reducer, concatenate into a vector.                                                                                                                                                                                                                                         
-   Note: same as (into [] coll), but parallel."
-  (r/fold (r/monoid into vector) conj coll))
 
 
-(defn fold-into-sorted-map-desc [coll]
-  "Provided a reducer, concatenate into a vector.                              
-   Note: same as (into [] coll), but parallel."
-   
-   ; todo: for map — (vec coll)
-   ; for other colls?
-  (r/fold (r/monoid conj #(sorted-map-by 
-                            (fn [key1 key2] (compare key2 key1)))) (vec coll)))
-        
-        
-; todo!
-;(defn fold-into-vec-format [coll f]
-;  "Provided a reducer, concatenate into a vector.                                                                                                                                                                                                                                         
-;   Note: same as (into [] coll), but parallel."
-;  (r/fold (r/monoid #(into (f %1) %2) vector) conj coll))
+#_(def log-file "/Users/ndrw/monte/new_sqls.log")
 
 
-; todo: add modifications of this
 
-(defn rindex-by-fn
-  "Uses core.reducers/fold to return a map indexed by the value of key-fn                                                                                                                                                                                                                 
-   applied to each element in coll.                                                                                                                                                                                                                                                       
-   Note: key-fn can return a collection of multiple keys."
-  ([key-fn coll ] (rindex-by-fn key-fn identity coll))
-  ([key-fn value-fn coll] (doall (clojure.core.reducers/fold
-                                  ;; combinef                                                                                                                                                                                                                                             
-                            (clojure.core.reducers/monoid (partial merge-with into)
-                                          hash-map)
-                                  ;; reducef                                                                                                                                                                                                                                              
-                                  (fn [ndx elem] (let [key (key-fn elem)]
-                                                   (if (coll? key)
-                                                     (reduce (fn [sndx selem] (assoc sndx selem (conj (get sndx selem [])
-                                                                                                      (value-fn elem))))
-                                                             ndx key)
-                                                     (assoc ndx key (conj (get ndx key [])
-                                                                    (value-fn elem))))))
-                            coll))))
-
-(def log-file "/Users/ndrw/monte/sqls.log")
-
-(with-open [rdr (time (clojure.java.io/reader log-file))]
-    (let [raw-data (time (json/read rdr))]
-      (let [sqls (time (get raw-data "sqls"))
-            ;reducer (comp (r/filter #(> 1 (- (get % "end") (get % "start"))) )  
-            ;              (r/map inc))
-            slow-seconds 0.5
-            
-            
-            ; good version
-            slow-sqls (r/filter #(> (- (get % "end") (get % "start")) slow-seconds ) sqls)
-            slow-vec (fold-into-vec slow-sqls)
-            
-            folded-by-sql (p-smart-merge 
-                            slow-vec 
-                            {:sql-fn (merger-by-key "sql"
-                                                    (fn [item]
-                                                       (assoc (dissoc item "sql" "result")
-                                                              :time (- (get item "end") (get item "start")))))})
-
-            
-            
-                        
+#_(with-open [rdr (clojure.java.io/reader log-file)]
+  (println "loading json")
+    (let [sqls (first (parsed-seq rdr)) ; (time (json/read rdr))
+          
+          slow-seconds 0.5
+          ; good version
+          ;slow-sqls (r/filter #(> (- (get % "end") (get % "start")) slow-seconds ) sqls)
+          
+          f-fn (filter-by-f 
+                         #(> (- (get % "end") (get % "start")) slow-seconds))
+          
+          slow-sqls1 (fold-into-vec (f-fn sqls))
+          
+          
+          ;slow-sqls1 (time ((fold-into-vec (filter-by-f #(> (- (get % "end") (get % "start")))) sqls)))
+          
+          folded-by-sql (p-smart-merge-v
+                          slow-sqls1
+                          [(merger-by-key "sql" (fn [item]
+                                                   (assoc (dissoc item "sql" "result")
+                                                          :time (- (get item "end") (get item "start")))))]
+                          )
+          
+          ;folded-by-sql (p-smart-merge 
+          ;                  slow-sqls1 
+          ;                  {:sql-fn (merger-by-key "sql"
+          ;                                          (fn [item]
+          ;                                             (assoc (dissoc item "sql" "result")
+          ;                                                    :time (- (get item "end") (get item "start")))))})
+           
+           
             ;slow-map (time (rindex-by-fn #(get % "sql") slow-sqls))
             ;report (time (fold-into-sorted-map-desc 
             ;         (map identity slow-map) ))
+          ]
+          
+          
+          ;(doall (time (println (take 10 slow-sqls1))))
+          
+          
+          
+          ;(time (doall (clojure.pprint/pprint folded-by-sql)))
+          ;(println "==============================")
+          ;(doall (println (pr-str slow-sqls)))
+          ; (doall (println folded-by-sql))
+          
+          ;(generate-stream folded-by-sql (clojure.java.io/writer "result.json"))
+          
+          (println 
+          (p-smart-merge-v
             
-            ]
-              
-              
-              (doall (clojure.pprint/pprint folded-by-sql))
-              ;(println "==============================")
-              ;(doall (println (pr-str slow-sqls)))
-              ;(doall (println folded-by-sql))
-              
-                     
-              #_(doall (map #(let [[sql details] %]
-                             
-                             (println sql)
-                             (println (count details))
-                             
-                             ) report))
-                     
-                                  
-              (comment ; works ok
-                           
-              (doall (map #(let [[sql details] %]
-                             
-                             (println sql)
-                             (println (count details))
-                             
-                             ) slow-map))
-               
-              )           
-                    
-              (comment ; works ok
-                (doall (map #(do
-                  (println 
-                    (str "took " 
-                      (pr-str (- (get % "end") (get % "start")))
-                       "seconds"))
-               (println (get % "sql"))
-               ) slow-vec)))
+            (get folded-by-sql "select max(joid) joid from jvjobs where pc=:pc")
+            [(aggregator-by-key :time +)]
+            )
+          )
+          
+                 
+          #_(doall (map #(let [[sql details] %]
+                         
+                         (println sql)
+                         (println (count details))
+                         
+                         ) report))
+                 
+                              
+          (comment ; works ok
+                       
+          (doall (map #(let [[sql details] %]
+                         
+                         (println sql)
+                         (println (count details))
+                         
+                         ) slow-map))
+           
+          )           
+                
+          (comment ; works ok
+            (doall (map #(do
+              (println 
+                (str "took " 
+                  (pr-str (- (get % "end") (get % "start")))
+                   "seconds"))
+           (println (get % "sql"))
+           ) slow-vec)))
 
       
         ;(reduce conj [] (r/map inc [1 2 3]))
@@ -241,4 +174,4 @@
       
       
 ;     (spit "sqls.edn" raw-data)
-      )))
+      ))
