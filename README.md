93555dace76cb7c9e545fd3b1582e5f86faee518
Andrew Sernyak <ndrew.sernyak@gmail.com>
2014-10-16 02:01:32 +0300

    0.0.3

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
        __  _______  _  ____________
       /  |/  / __ \/ |/ /_  __/ __/
-     / /|_/ / /_/ /    / / / / _/  
-    /_/  /_/\____/_/|_/ /_/ /___/ 
+     / /|_/ / /_/ /    / / / / _/
+    /_/  /_/\____/_/|_/ /_/ /___/
 
 A software visualization tool written in clojure for my master's degree thesis
 
@@ -11,24 +11,7 @@ A software visualization tool written in clojure for my master's degree thesis
 
 ## Usage
 
-Clone the repo and do
-
-       lein run 
-
-if you want to have jar do
-
-       lein uberjar
-       java -jar target/monte-0.0.0-SNAPSHOT-standalone.jar 
-
-## Command line switches
-
-    Switches                         Default  Desc              
-    --------                         -------  ----              
-    -p, --port                       8899     Port to listen on 
-    -a, --no-auto-open, --auto-open  false                      
-    -c, --no-color, --color          false                      
-    -d, --no-debug, --debug          false                      
-    -h, --no-help, --help            false    Show help  
+Use as a library :)
 
 ## Versions
 
@@ -36,6 +19,6 @@ Current: pre-alpha, API and organizational structure are subject of change.
 
 ## License
 
-Copyright © 2012-2013, Andrew Sernyak
+Copyright © 2012-2014, Andrew Sernyak
 
 Distributed under the Eclipse Public License, the same as Clojure.
--- project.clj
+++ project.clj
@@ -1,37 +1,21 @@
-(defproject monte "0.0.2"
+(defproject monte "0.0.3"
   :description "Monte is a software visualisation tool created as
             	a part of Andrew Sernyak master degree' thesis"
   :url "https://github.com/ndrew/monte"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojure-contrib "1.2.0"]
-                 [org.clojure/core.logic "0.7.5"]
-
-                 [org.clojure/data.json "0.2.2"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/core.logic "0.7.5"] ; 0.8.8 - fail datalog
+                 [org.clojure/data.json "0.2.5"]
 
                  [datalog               "0.1.1"]
-                 ; use new version of it
                  [me.raynes/fs          "1.4.4"]
-                 [compojure             "1.1.3"]
-                 [lib-noir              "0.3.3"]
-                 [org.clojure/tools.cli "0.2.2"]
-                 [crate                 "0.2.4"]
-                 ; avaiting better times
-                 ;[prismatic/dommy       "0.0.2"]
-                 [jayq                  "2.0.0"]
-                 [shoreleave            "0.2.2"]
-                 [com.cemerick/shoreleave-remote-ring "0.0.2" ] ; 0.0.3
+                 [org.clojure/tools.cli "0.3.1"]
                  [myguidingstar/clansi "1.3.0"]
-                 [sandbar/sandbar "0.3.3"]
-                 [cheshire "5.1.1"]
-
-                 [org.clojure/tools.namespace "0.2.3"]
-                 [criterium "0.4.0"]
-
-                 [net.drib/strokes "0.4.1"]
-
+                 [cheshire "5.3.1"]
+                 [org.clojure/tools.namespace "0.2.7"]
+                 [criterium "0.4.3"]
                  [org.codehaus.jsr166-mirror/jsr166y "1.7.0"]
                  ]
 
